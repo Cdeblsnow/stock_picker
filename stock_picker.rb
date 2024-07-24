46e4e12076ec88db610349518cfd60686faef80c
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-07-24 01:08:58 -0400

    Fix error giving incorrect profit result
    Wrong day combination persist

--- stock_picker.rb
+++ stock_picker.rb
@@ -9,19 +9,23 @@ def stock_prices(array)
   best_combination = []
   highest_index = 0
   lowest_index = 0
+  lowest_buy = 0
+  highest_sell = 0
 
   array.length.times do |counter|
     lowest_buy = array.sort[counter]
     lowest_index = array.find_index(lowest_buy)
 
     array.length.times do |iterator|
-      highest_sell = array.sort[-counter]
+      highest_sell = array.sort[-iterator]
       highest_index = array.find_index(highest_sell)
       profit = highest_sell - lowest_buy 
-      max_profit = profit if  profit > max_profit &&  lowest_index < highest_index 
+      max_profit = profit if  profit > max_profit &&  lowest_index < highest_index
+      max_profit 
     end 
   end
   best_combination = [lowest_index,highest_index]
   p "The best combination is #{best_combination} for a profit of #{max_profit}"
 end 
 
+stock_prices(prices_array)
