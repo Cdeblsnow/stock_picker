b075b319e88210c36bcb03d593e1f806705cdf32
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-07-24 00:03:01 -0400

    Create logic for stock picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,27 @@
 puts "Please enter the prices by day"
 prices_array = gets.chomp.split(",")
-p prices_array
 prices_array = prices_array.map{|number| number.to_i}
-p prices_array
-  
+
+def stock_prices(array)
+
+  profit = 0
+  lowest_buy = array.min 
+  highest_sell = array.max
+  best_combination = []
+
+  array.length.times do |counter|
+    highest_sell = array.sort(-counter)
+    
+    if array.find_index(highest_sell) > array.find_index(lowest_buy)
+      profit = highest_sell - lowest_buy
+      best_combination = [array.find_index(lowest_buy), array.find_index(highest_sell)]
+      p "#{best_combination} for a profit of #{profit}"
+      break
+        
+    elsif array.find_index(highest_sell) < array.find_index(lowest_buy)
+      p "There is not best day to sell"
+    
+    end
+  end 
+end 
 
